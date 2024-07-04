0b65725b7d85190903a2d68ae5967d342bd66aa6
james_ma <t90laser93@gmail.com>
2024-07-04 19:10:06 +0900

    typedef, using으로 type 별칭 생성하기

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
-# include <cmath> // 수학 관련 기능을 제공해주는 library
+#include <cmath> // 수학 관련 기능을 제공해주는 library
+#include <vector>
 
-using namespace std;
 
-// 별도의 namespace(JavaScript scope같은 느낌인가?)를 선언하면 같아보이는 변수명을 사용할 수 있음.
-// chatGPT에게 물어본 결과
-/* Namespaces are a feature in C++ designed to organize code into logical groups and to
-// prevent name collisions that can occur especially when your code base includes multiple
-// libraries. A namespace defines a scope that contains a set of related names, such as
-variables, functions, and classes. */
+/*
+typedef는 C++에서 데이터 타입에 새로운 이름을 부여하는 키워드입니다. 이를 통해 복잡한 타입을 간단하게 표현하거나, 코드의 가독성을 높일 수 있습니다.
+주요 특징:
+
+- 기존 타입의 별칭 생성
+- 복잡한 타입 선언 간소화
+- 코드 유지보수 용이성 향상
+
+newTypeName_t 형식으로 뒤에 `_t`라는 suffix를 붙여서 이름에서 typedef를 알 수 있도록 하는 것이 컨벤션이다.
+ */
+
 namespace MyNameSpace {
    int x = 1;
 }
@@ -19,6 +24,23 @@ namespace AnotherNameSpace {
 }
 
 
+
+using namespace std;
+
+// 별도의 namespace(JavaScript scope같은 느낌인가?)를 선언하면 같아보이는 변수명을 사용할 수 있음.
+// chatGPT에게 물어본 결과
+/* Namespaces are a feature in C++ designed to organize code into logical groups and to
+// prevent name collisions that can occur especially when your code base includes multiple
+// libraries. A namespace defines a scope that contains a set of related names, such as
+variables, functions, and classes. */
+
+typedef std::vector<std::pair<std::string,int>> pairlist_t;
+// typedef std::string text_t;
+
+// 하지만 typedef는 요즘 거의 using키워드로 대체되었다. 왜냐하면 using 키워드가 tempale으로 처리하기 더 좋기 때문이다.
+// 강의자도 using키워드 사용하라고 추천함
+// 명시적으로 benefit이 있을때만 꼭 사용하라고 추천함
+using new_text_t = std::string;
 
 int main() {
    const double result = floor(1.2);
@@ -66,6 +88,17 @@ int main() {
    // 가능성이 매우 크다고 한다.
    // using namespace std보다는, using namespace std::cout, using namespace std::string 과 같이 선언해서 쓰는것이
    // 더 안전한 alternative
+
+   // 생성한 typedef를 활용해서 기존에 존재하는 타입에 새로운 구분자를 부여한 것을 사용해보자.
+   pairlist_t pairlist;
+
+   // std::string firstname;
+   // text_t firstname = "Bro";
+   // cout << firstname;
+
+   new_text_t myName = "ji";
+
+   cout << myName;
 
    return 0;
 }
